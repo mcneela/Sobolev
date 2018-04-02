b2ff0eaccb741255ccdafeb961af92890fedb6fe
mcneela <daniel.mcneela@gmail.com>
2018-04-01 20:29:20 -0700

    Added bias terms to model

--- src/model.py
+++ src/model.py
@@ -5,16 +5,18 @@ class SobolevNetwork:
         self.input_dim = input_dim 
         self.num_hidden = num_hidden
         self.W1 = tf.Variable(tf.random_normal([self.input_dim, self.num_hidden],
-                                  stddev=0.1))
+                                   stddev=0.1))
+        self.b1 = tf.Variable(tf.ones([self.num_hidden]))
         self.W2 = tf.Variable(tf.random_normal([self.num_hidden, self.num_hidden],
                                    stddev=0.1))
+        self.b2 = tf.Variable(tf.ones([self.num_hidden]))
         self.W3 = tf.Variable(tf.random_normal([self.num_hidden, 1],
                                    stddev=0.1))
-        self.weights = [self.W1, self.W2, self.W3]
+        self.b3 = tf.Variable(tf.ones([1]))
+        self.weights = [(self.W1, self.b1), (self.W2, self.b2), (self.W3, self.b3)]
 
     def forward(self, X):
         out = X
-        for W in self.weights[:-1]:
-            out = tf.nn.relu(tf.matmul(out, W))
-        out = tf.matmul(out, self.weights[-1])
+        for W, b in self.weights:
+            out = tf.nn.relu(tf.matmul(out, W) + b)
         return out
